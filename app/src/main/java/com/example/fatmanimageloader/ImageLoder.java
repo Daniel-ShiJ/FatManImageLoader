3560d4ad3c6f6ea0ef68540204008103c450e00e
A flexible fat man <57322158@qq.com>
2020-09-10 17:03:35 +0800

    first commit.

---
+++ app/src/main/java/com/example/fatmanimageloader/ImageLoder.java
@@ -0,0 +1,8 @@
+package com.example.fatmanimageloader;
+
+/**
+ * Created by Fat Man
+ * on 2020/8/13
+ */
+public class ImageLoder {
+}
---
+++ app/src/main/java/com/example/fatmanimageloader/ImageResizer.java
@@ -0,0 +1,22 @@
+package com.example.fatmanimageloader;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+
+/**
+ * Created by Fat Man
+ * 压缩图片
+ * on 2020/8/13
+ */
+public class ImageResizer {
+    private static final String TAG = "ImageResizer";
+
+    ImageResizer(){
+
+    }
+
+    public Bitmap decodeSampledBitmapFromResource(Resources res,int resId,int reqWidth,int reqHeight){
+
+    }
+
+}
